75b252de48be3356cb0b0ffbf6a310e289b8ba50
Dmitry Glyzin <glyzin@gmail.com>
2024-02-22 12:42:35 +0000

    basice structure

---
+++ ancinf/pipeline.py
@@ -0,0 +1,9 @@
+
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("echo", help="echo the string you use here")
+    args = parser.parse_args()
+    print(args.echo)
