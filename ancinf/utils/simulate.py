fbb0de404591b4427c620d5576f7488f25d53582
Dmitry Glyzin <glyzin@gmail.com>
2024-03-22 17:05:34 +0000

    fixes for simulate

--- ancinf/utils/simulate.py
+++ ancinf/utils/simulate.py
@@ -1,26 +1,62 @@
 import json
 import os
+import pandas as pd
+#from .genlink import get_classes, compute_simulation_params_fn
+from os import listdir
+from os.path import isfile, join
 
-def collectparams(folder):
-    #get every csv in the folder and compute its parameters
-    #maybe some other parameters
+from .baseheuristic import getprobandmeanmatrices, composegraphs, checkpartition
+from .ibdloader import load_pure
+
+
+def collectparams(folder, override_popsizes):
+    '''
+    get every csv in the folder and compute its parameters
+    
     dummy = {"CR.csv": {"pop_names":["pop1","pop2","pop3"],
                         "pop_sizes":[1000,1000,1000],
-                        "edge_probability":[[1,0,0],[0,1,0],[0,0,1]]
+                        "edge_probability":[[1,0,0],[0,1,0],[0,0,1]],
+                        "mean_weight":[[1,0,0],[0,1,0],[0,0,1]]
                        },
              "NC.csv": {"pop_names":["pop1","pop2","pop3"],
                         "pop_sizes":[1000,1000,1000],
-                        "edge_probability":[[1,0,0],[0,1,0],[0,0,1]]
+                        "edge_probability":[[1,0,0],[0,1,0],[0,0,1]],
+                        "mean_weight":[[1,0,0],[0,1,0],[0,0,1]]
                        }
             }
-    return dummy
+    '''
+    
+    filelist = [f for f in listdir(folder) if isfile(join(folder, f))]
+    csvlist = [f for f in filelist if f[-3:] == 'csv']
+    
+    with open(join(folder, "meta.json"),"r") as f:
+        meta = json.load(f)
+    
+    for fname in csvlist:        
+        print("processing file", fname)
+        fnamepath = join(folder, fname)
+        pairs, weights, labels, labeldict, idxtranslator = load_pure( fnamepath, **meta[fname] )
+        print("load ok!")
+        graphdata = composegraphs(pairs, weights, labels, labeldict, idxtranslator)
+        ncls = graphdata[0]['nodeclasses']
+        grph = graphdata[0]['graph']
+        trns = graphdata[0]['translation']
+
+        checkpartition(grph, ncls, None, None, details=True, trns=trns)
+
+        means, probs  = getprobandmeanmatrices(grph, ncls, labeldict)
+        print("means:", means)
+        print("probs:", probs)
+        
+        
+    return "dummy"
 
     
 def collectandsaveparams(folder, outfile, override_popsizes):
     print(f"Collecting parameters for datasets from {folder}")
     if not(override_popsizes is None):
         print(f"Will save population sizes = {override_popsizes} instead of original")
-    collected = collectparams(folder)    
+    collected = collectparams(folder, override_popsizes)    
     with open(outfile,"w") as f:
         json.dump(collected,f)
 
@@ -45,5 +81,5 @@ def simulateandsave(paramfile, outfolder):
         
     
 
-if __name__=="__main":
+if __name__=="__main__":
     print("just a test")
