7eba465010fae3cc6e2e6be9442a66c5472f97ac
Dmitry Glyzin <glyzin@gmail.com>
2024-05-02 19:55:53 +0000

    test example

---
+++ test/run_all.sh
@@ -0,0 +1,7 @@
+echo Hello!
+echo 1. Check preprocessing
+python3 -m ancinf preprocess ./datasets/ ./workdir/test1/ --infile arrakis.ancinf
+echo 2. Check getting parameters
+python3 -m ancinf getparams ./datasets/ ./workdir/test2/ --infile arrakis.ancinf
+echo 3. Check simulations
+python3 -m ancinf simulate ./workdir/test3/ --infile arrakis.params
