e89eed06d8ce0c79ba385d1daac77460979e48e4
Dmitry Glyzin <glyzin@gmail.com>
2024-05-09 08:01:02 +0000

    fix

--- ancinf/utils/simulate.py
+++ ancinf/utils/simulate.py
@@ -556,7 +556,9 @@ def simplified_genlink_run(dataframe_path, train_split, valid_split, test_split,
                       'one_hot', 1, 1, cuda_device_specified=gpu)
     else:
         #mlps
-        dp.make_train_valid_test_datasets_with_numba('graph_based', 'homogeneous', 'multiple', 'multiple', rundir, log_edge_weights=logweights)    
+        #print(f"Log weignts: {logweights}")
+        #dp.make_train_valid_test_datasets_with_numba('one_hot', 'homogeneous', 'multiple', 'multiple', rundir, log_edge_weights=logweights)    
+        dp.make_train_valid_test_datasets_with_numba('graph_based', 'homogeneous', 'one', 'multiple', rundir, log_edge_weights=logweights)    
         trainer = Trainer(dp, nnclass, 0.0001, 5e-5, torch.nn.CrossEntropyLoss, 10, rundir, 2, 20,
                       'graph_based', 10, 1, cuda_device_specified=gpu)
     
